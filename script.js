ab5078dd5520d3e5d25c1b8c77bea2600948ec8e
Gitko <53922557+Gitko01@users.noreply.github.com>
2022-09-04 19:48:43 -0500

    Add multiple versions for direct downloads
    + Modified page background color switcher thingy

--- script.js
+++ script.js
@@ -15,8 +15,8 @@ async function bgColorChange(mode) {
     let done = false;
 
     if (mode == true) {
-        startColor = 50
-        endColor = 35
+        startColor = 52
+        endColor = 34
 
         currentColor = startColor;
 
@@ -39,14 +39,14 @@ async function bgColorChange(mode) {
             document.body.style.backgroundColor = `rgb(${currentColor}, ${currentColor}, ${currentColor})`
 
             if (currentColor > endColor && done == false) {
-                currentColor -= 1
+                currentColor -= 2
                 document.body.style.backgroundColor = `rgb(${currentColor}, ${currentColor}, ${currentColor})`
             } else if (currentColor == endColor) {
                 done = true;         
             }
 
             if (currentColor < startColor && done == true) {
-                currentColor += 1
+                currentColor += 2
                 document.body.style.backgroundColor = `rgb(${currentColor}, ${currentColor}, ${currentColor})`
             } else if (currentColor == startColor) {
                 done = false;
@@ -80,66 +80,106 @@ function changeColorMode() {
     if (darkMode) {
         // light mode
         darkMode = false;
-        document.getElementById('page-title').style.color = "#0e0e0e"
-
-        for (let i = 0; i < document.getElementsByClassName('subtitle').length; i++) {
-            document.getElementsByClassName('subtitle')[i].style.color = "#0e0e0e"
+        if (document.getElementById('page-title')) {
+            document.getElementById('page-title').style.color = "#0e0e0e"
+        } else {
+            alert('no page title!')
         }
 
-        for (let i = 0; i < document.getElementsByClassName('title-left').length; i++) {
-            document.getElementsByClassName('title-left')[i].style.color = "#0e0e0e"
+        if (document.getElementsByClassName('subtitle')) {
+            for (let i = 0; i < document.getElementsByClassName('subtitle').length; i++) {
+                document.getElementsByClassName('subtitle')[i].style.color = "#0e0e0e"
+            }
+        }
+        
+        if (document.getElementsByClassName('title-left')) {
+            for (let i = 0; i < document.getElementsByClassName('title-left').length; i++) {
+                document.getElementsByClassName('title-left')[i].style.color = "#0e0e0e"
+            }
+        }
+        
+        if (document.getElementsByClassName('title-mid')) {
+            for (let i = 0; i < document.getElementsByClassName('title-mid').length; i++) {
+                document.getElementsByClassName('title-mid')[i].style.color = "#0e0e0e"
+            }
         }
 
-        for (let i = 0; i < document.getElementsByClassName('title-mid').length; i++) {
-            document.getElementsByClassName('title-mid')[i].style.color = "#0e0e0e"
+        if (document.getElementsByClassName('project')) {
+            for (let i = 0; i < document.getElementsByClassName('project').length; i++) {
+                document.getElementsByClassName('project')[i].style.backgroundColor = 'grey'
+                document.getElementsByClassName('project')[i].style.border = '3px solid #484848'
+            }
         }
 
-        for (let i = 0; i < document.getElementsByClassName('project').length; i++) {
-            document.getElementsByClassName('project')[i].style.backgroundColor = 'grey'
-            document.getElementsByClassName('project')[i].style.border = '3px solid #484848'
+        if (document.getElementsByClassName('textSection')) {
+            for (let i = 0; i < document.getElementsByClassName('textSection').length; i++) {
+                document.getElementsByClassName('textSection')[i].style.backgroundColor = 'grey'
+                document.getElementsByClassName('textSection')[i].style.border = '3px solid #484848'
+            } 
         }
 
-        for (let i = 0; i < document.getElementsByClassName('textSection').length; i++) {
-            document.getElementsByClassName('textSection')[i].style.backgroundColor = 'grey'
-            document.getElementsByClassName('textSection')[i].style.border = '3px solid #484848'
+        if (document.getElementById('colorModeButton')) {
+            document.getElementById('colorModeButton').textContent = "Dark Mode"
         }
-
-        document.getElementById('colorModeButton').textContent = "Dark Mode"
         document.body.style.backgroundColor = "white"
 
         bgColorChange(darkMode)
     } else {
         // dark mode
         darkMode = true;
-        document.getElementById('page-title').style.color = "white"
 
-        for (let i = 0; i < document.getElementsByClassName('subtitle').length; i++) {
-            document.getElementsByClassName('subtitle')[i].style.color = "white"
+        if (document.getElementById('page-title')) {
+            document.getElementById('page-title').style.color = "white"
+        } else {
+            alert('no page title!')
         }
 
-        for (let i = 0; i < document.getElementsByClassName('title-left').length; i++) {
-            document.getElementsByClassName('title-left')[i].style.color = "white"
+        if (document.getElementsByClassName('subtitle')) {
+            for (let i = 0; i < document.getElementsByClassName('subtitle').length; i++) {
+                document.getElementsByClassName('subtitle')[i].style.color = "white"
+            }
+        }
+        
+        if (document.getElementsByClassName('title-left')) {
+            for (let i = 0; i < document.getElementsByClassName('title-left').length; i++) {
+                document.getElementsByClassName('title-left')[i].style.color = "white"
+            }
+        }
+        
+        if (document.getElementsByClassName('title-mid')) {
+            for (let i = 0; i < document.getElementsByClassName('title-mid').length; i++) {
+                document.getElementsByClassName('title-mid')[i].style.color = "white"
+            }
         }
 
-        for (let i = 0; i < document.getElementsByClassName('title-mid').length; i++) {
-            document.getElementsByClassName('title-mid')[i].style.color = "white"
+        if (document.getElementsByClassName('project')) {
+            for (let i = 0; i < document.getElementsByClassName('project').length; i++) {
+                document.getElementsByClassName('project')[i].style.backgroundColor = '#141414'
+                document.getElementsByClassName('project')[i].style.border = '3px solid #484848'
+            }
         }
 
-        for (let i = 0; i < document.getElementsByClassName('project').length; i++) {
-            document.getElementsByClassName('project')[i].style.backgroundColor = '#141414'
-            document.getElementsByClassName('project')[i].style.border = '3px solid #484848'
+        if (document.getElementsByClassName('textSection')) {
+            for (let i = 0; i < document.getElementsByClassName('textSection').length; i++) {
+                document.getElementsByClassName('textSection')[i].style.backgroundColor = '#141414'
+                document.getElementsByClassName('textSection')[i].style.border = '3px solid #484848'
+            }
         }
 
-        for (let i = 0; i < document.getElementsByClassName('textSection').length; i++) {
-            document.getElementsByClassName('textSection')[i].style.backgroundColor = '#141414'
-            document.getElementsByClassName('textSection')[i].style.border = '3px solid #484848'
+        if (document.getElementById('colorModeButton')) {
+            document.getElementById('colorModeButton').textContent = "Light Mode"
         }
-
-        document.getElementById('colorModeButton').textContent = "Light Mode"
+       
         document.body.style.backgroundColor = "#323232"
 
         bgColorChange(darkMode)
     }
+}
+
+function submitDownload(optionsElementID) {
+    const OPTIONS_ELEMENT = document.getElementById(optionsElementID)
+    const SELECTED_OPTION = OPTIONS_ELEMENT.value
+    window.location = SELECTED_OPTION
 }
 
 if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
